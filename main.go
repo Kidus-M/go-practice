8f33b340927eff092996f8d253832412058ea9e1
kidus-m <kidus0237@gmail.com>
2025-11-05 20:27:58 +0300

    task 1

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func SumOfNumbers(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
 func main() {
-	fmt.Println("Hello, World")
+	fmt.Println(SumOfNumbers([]int{1, 2, 3, 4, 5}))
+	fmt.Println(SumOfNumbers([]int{}))
 }
